dea9c2e74afeffb7230a41e47e2442f4039cfb31
irwantowibowotech <irwantowibowotech@gmail.com>
2025-07-28 07:52:11 +0700

    setup ORM with gorm

--- internal/api/server.go
+++ internal/api/server.go
@@ -4,18 +4,33 @@ import (
 	"go-ecommerce-app/config"
 	"go-ecommerce-app/internal/api/rest"
 	"go-ecommerce-app/internal/api/rest/handlers"
+	"go-ecommerce-app/internal/domain"
+	"log"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func StartServer(config config.AppConfig) {
 	app := fiber.New()
 
+	db, err := gorm.Open(postgres.Open(config.Dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("database connection server error : %v", err)
+	}
+
+	log.Println("database connected")
+
+	// run migrations
+	db.AutoMigrate(&domain.User{})
+
 	app.Get("/health", HealtCheck)
 
 	restHandler := &rest.RestHandler{
 		App: app,
+		DB:  db,
 	}
 
 	setupRoutes(restHandler)
